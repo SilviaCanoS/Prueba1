7a29eabc14564a4057d851087ebaab5b598ddfba
SilviaCanoS <99064925+SilviaCanoS@users.noreply.github.com>
2023-05-14 22:13:32 -0600

    Hola Mundo
    Crea un script que imprime en consola Hola Mundo

---
+++ Unity2/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print("Algo pasó"); //informativo, requiere la palabra MonoBehaviour despues de la clase
+        Debug.Log("Hola Mundo"); //informativo
+        //Debug.LogError("Algo salio muy mal"); //Error
+        //Debug.LogWarning("Algo puede salir mal"); //Advertencia
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
